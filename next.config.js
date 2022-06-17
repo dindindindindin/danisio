49f2a1cca12c46f3a305c61d7995fed9fb0b72da
dindindindindin <dincercetiner@hotmail.com>
2022-06-17 18:06:10 +0300

    nottin

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const { i18n } = require("./next-i18next.config");
 // const { nextI18NextRewrites } = require("next-i18next/rewrites");
 // const localeSubpaths = {
-//   de: "de",
 //   en: "en",
+//   tr: "tr",
 // };
 module.exports = {
   i18n,
